342d88592fb353219c2775c6969ae7d1e788549d
Vlad Muslimov <muslimov.vlad@mail.ru>
2023-04-02 12:44:59 +0500

    Home work 1 (Heap sort)

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import homeWork1.HeapSort;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{
+                5, 2, 8, 1, 2, 9, 5, 2, 0, 10
+        };
+        printArray(array);
+        HeapSort.sort(array);
+        printArray(array);
+    }
+
+    private static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
