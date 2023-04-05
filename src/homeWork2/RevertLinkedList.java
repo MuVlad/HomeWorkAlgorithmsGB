5a021426ca0e083f9d5f2113cb1916dc3ab50f8b
Vlad Muslimov <muslimov.vlad@mail.ru>
2023-04-05 22:56:03 +0500

    Home work 2 (reverse linked list)

---
+++ src/homeWork2/RevertLinkedList.java
@@ -0,0 +1,30 @@
+package homeWork2;
+
+public class RevertLinkedList {
+
+    Node head;
+    Node tail;
+
+    public void revert() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            Node next = currentNode.next;
+            Node previous = currentNode.previous;
+            currentNode.next = previous;
+            currentNode.previous = next;
+            if (previous == null) {
+                tail = currentNode;
+            }
+            if (next == null) {
+                head = currentNode;
+            }
+            currentNode = next;
+        }
+    }
+
+    public class Node {
+        int value;
+        Node next;
+        Node previous;
+    }
+}
